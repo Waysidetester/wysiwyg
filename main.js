0767a25ec9d4ab4df8b4510b3e9656e31f20e718
WaysideTester <dylanmurray2119@gmail.com>
2018-09-24 21:26:23 -0500

    array WIP

---
+++ main.js
@@ -0,0 +1,87 @@
+const famous = [
+    {
+        title: "Standard Oil",
+        name: "John D. Rockefeller",
+        bio: "Founding and leading the Standard Oil Company, founding the University of Chicago, Rockefeller University, Central Philippine University, General Education Board, and Rockefeller Foundation",
+        netWorth: "$400 billion (in 2017 dollars; inflation-adjusted)",
+        image: "https://upload.wikimedia.org/wikipedia/commons/6/6f/John_D._Rockefeller_1885.jpg",
+        lifespan: {
+            birth: 1839,
+            death: 1937 
+            }
+    },
+    {
+        title: "Bethlehem Steel",
+        name: "Charles M. Schwab",
+        bio: "Charles Michael Schwab was an American steel magnate. Under his leadership, Bethlehem Steel became the second largest steel maker in the United States, and one of the most important heavy manufacturers in the world.",
+        netWorth: "$6.2 Billion (in 2017 dollars; inflation-adjusted)",
+        image: "https://upload.wikimedia.org/wikipedia/commons/0/08/Charles_M._Schwab_cph.3b33668.jpg",
+        lifespan: {
+            birth: 1862,
+            death: 1939 
+        }
+    },
+    {
+        title: " New York Central Railroad",
+        name: "Cornelius Vanderbilt",
+        bio: "Cornelius Vanderbilt (May 27, 1794 – January 4, 1877) was an American business magnate and philanthropist who built his wealth in railroads and shipping. Born poor and having only a mediocre education, Vanderbilt used perseverance, intelligence and luck to work his way into leadership positions in the inland water trade and invest in the rapidly growing railroad industry. Nicknamed \"The Commodore\", he is known for owning the New York Central Railroad.",
+        netWorth: "$215 billion (2016 dollars; inflation-adjusted)",
+        image: "https://upload.wikimedia.org/wikipedia/commons/5/57/Cornelius_Vanderbilt_Daguerrotype2.jpg",
+        lifespan: {
+            birth: 1794,
+            death: 1877 
+        }
+    },
+    {
+        title: "First Transcontinental Railroad",
+        name: "Mark Hopkins Jr.",
+        bio: "he founded the Central Pacific Railroad. Sometimes called \"Uncle Mark\", he was the eldest of the four partners and was well known for his thriftiness (it was said that he knew how to \"squeeze 106 cents out of every dollar\", a reputation that gained him the post of company treasurer. Noted American historian Hubert Howe Bancroft quotes Collis Huntington as saying, \"I never thought anything finished until Hopkins looked at it\". Bancroft described Hopkins as the \"balance-wheel of the Associates and one of the truest and best men that ever lived.\"",
+        netWorth: "$1 billion (2016 dollars; inflation-adjusted)",
+        image: "https://upload.wikimedia.org/wikipedia/commons/1/1e/Mark_Hopkins_1813-1878.jpg",
+        lifespan: {
+            birth: 1813,
+            death: 1878
+        }
+    },
+    // stopped here. PICK BACK UP HERE
+    {
+        title: "Samurai",
+        name: "Tomoe Gozen",
+        bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+        image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+        lifespan: {
+            birth: 1747,
+            death: 1797
+        }
+    },
+    {
+        title: "Samurai",
+        name: "Tomoe Gozen",
+        bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+        image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+        lifespan: {
+            birth: 1747,
+            death: 1797
+        }
+    },
+    {
+        title: "Samurai",
+        name: "Tomoe Gozen",
+        bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+        image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+        lifespan: {
+            birth: 1747,
+            death: 1797
+        }
+    },
+    {
+        title: "Samurai",
+        name: "Tomoe Gozen",
+        bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+        image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+        lifespan: {
+            birth: 1747,
+            death: 1797
+        }
+    }
+];
